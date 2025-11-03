597fadb5b3e7167ba521cf3ce52e447d381d0f01
Vijay Bommireddy <bvkreddy543@gmail.com>
2025-11-02 22:57:01 -0500

    This is the final change

--- application.py
+++ application.py
@@ -36,6 +36,6 @@ def predict():
         return str(e)
     
 if __name__=="__main__":
-    app.run(debug=True , host="0.0.0.0" , port=5000)
+    app.run(debug=True , host="0.0.0.0" , port=5600)
 
     
